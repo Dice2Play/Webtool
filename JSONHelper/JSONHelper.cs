d203630074ab9a1d1fef848b43c5dbc05607fe4b
Tim Scholtens <timscholtens@hotmail.com>
2018-01-09 16:59:26 +0100

    Added JSON Serializer

---
+++ JSONHelper/JSONHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace JSONHelper
+{
+    public static partial class JSONHelper
+    {
+
+        public static HttpCookie DeserializeToCookie(string rawJSONString)
+        {
+            var cookie = JsonConvert.DeserializeObject<HttpCookie>(rawJSONString);
+
+            return cookie;
+        }
+
+
+
+    }
+}
--- WebHelper/WebHelper/Browser/WebHelper.cs
+++ WebHelper/WebHelper/Browser/WebHelper.cs
@@ -15,14 +15,64 @@ namespace WebHelperProject
     // This class is responsible for all browser related tasks
     public static partial class WebHelper
     {
+        #region Public methods
+        /// <summary>
+        /// Opens Uri in browser using selenium library.
+        /// </summary>
+        /// <param name="urlToOpen">Uri to open.</param>
         public static void OpenWebPageInBrowser(Uri urlToOpen)
         {
-            var chromeDriverApplicationPath = $"{Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ChromeDriver")}";
-
-            ChromeDriver chromeDriver = new ChromeDriver(@chromeDriverApplicationPath);
-            chromeDriver.Navigate().GoToUrl("https://www.google.nl");
-
+            // Browse to input uri
+            ReturnChromeDriver().Navigate().GoToUrl(urlToOpen);
         }
+
+        /// <summary>
+        /// Opens Uri in browser using selenium library.
+        /// </summary>
+        /// <param name="urlToOpen">Uri to open.</param>
+        public static void OpenWebPageInBrowser(Uri urlToOpen, HttpCookie cookiesToUse)
+        {
+            // Set cookie(s)
+
+
+            // Browse to input uri
+            ReturnChromeDriver().Navigate().GoToUrl(urlToOpen);
+        }
+
+        /// <summary>
+        /// Opens Uri in browser using selenium library.
+        /// </summary>
+        /// <param name="urlToOpen">Uri to open.</param>
+        public static void OpenWebPageInBrowser(Uri urlToOpen, HttpCookie cookiesToUse, UserAgent userAgentToUse )
+        {
+            // Set cookie(s)
+
+
+            // Set user agent
+
+
+            // Browse to input uri
+            ReturnChromeDriver().Navigate().GoToUrl(urlToOpen);
+        }
+
+
+
+
+        #endregion
+
+
+        #region Private methods
+
+        private static ChromeDriver ReturnChromeDriver()
+        {
+            var chromeDriverApplicationPath = $"{Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ChromeDriver")}";
+            return new ChromeDriver(@chromeDriverApplicationPath);
+        }
+
+        #endregion
+
+
+
 
     }
 }
